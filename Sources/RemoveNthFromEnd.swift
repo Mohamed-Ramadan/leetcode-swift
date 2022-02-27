681064eb997ab20da487068852a5a5e5aed38661
Mohamed Ramadan <mohamed.ramadan@nagwa.com>
2022-02-27 16:02:48 +0200

    solve 2 linked list problems

--- Sources/RemoveNthFromEnd.swift
+++ Sources/RemoveNthFromEnd.swift
@@ -1,2 +1,41 @@
 import Foundation
 
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+class RemoveNthFromEndSolution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        var nodes: [ListNode?] = []
+        var current = head
+        
+        while current != nil {
+            nodes.append(current)
+            current = current?.next
+        }
+        
+        nodes[nodes.count-n-1]?.next = nodes[n]?.next
+
+        return head
+    }
+    
+    func removeNthFromEnd2(_ head: ListNode?, _ n: Int) -> ListNode? {
+        var fast: ListNode? = head
+        var slow: ListNode? = head
+        var n = n
+        
+        while n != 0 {
+            fast = fast?.next
+            n -= 1
+        }
+         
+        while fast?.next != nil {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        
+        var temp = slow?.next
+        slow?.next = slow?.next?.next
+        temp = nil
+        
+        return head
+    }
+}
--- Sources/SwapNodes.swift
+++ Sources/SwapNodes.swift
@@ -1,2 +1,30 @@
 import Foundation
 
+class Solution {
+    func SwapNodes(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var fast = head
+        var slow =  head
+        var slowTemp: ListNode?
+        var k = k
+        
+        while k != 0 {
+            fast = fast?.next
+            k -= 1
+        }
+        
+        slowTemp = fast
+        
+        while fast?.next != nil {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        
+        if let tempVal = slow?.val, let slowTempVal = slowTemp?.val {
+            slow?.val = slowTempVal
+            slowTemp?.val = tempVal
+        }
+         
+        return head
+    }
+    
+}
